60438f4b0d37468733c70f81f8a7b36e101e2227
Paul James <paul@peej.co.uk>
2010-01-24 10:34:14 +0000

    Search improvements

--- controllers/search.py
+++ controllers/search.py
@@ -12,14 +12,10 @@ class SearchPage(Controller):
         lat = round(lat, 1)
         lon = round(lon, 1)
         
-        lats = [lat - 0.1, lat, lat + 0.1]
-        lons = [lon - 0.1, lon, lon + 0.1]
+        lats = [lat, lat + 0.1]
+        lons = [lon, lon + 0.1]
         
         attractions = []
-        
-        markerCount = 0
-        markerColour = 0;
-        markerColours = ['red', 'green', 'yellow', 'blue']
         
         for latitude in lats:
             for longitude in lons:
@@ -32,17 +28,17 @@ class SearchPage(Controller):
                 
                 if geoboxes:
                     for attractionId in geoboxes.attractions:
-                        markerCount = markerCount + 1
-                        if markerCount > 26:
-                            markerCount = 1
-                            markerColour = markerColour + 1
                         attraction = Attraction.all()
                         attraction.filter("id =", attractionId)
-                        a = attraction.get()
-                        a.marker = chr(markerCount + 64)
-                        a.colour = markerColours[markerColour]
-                        attractions.append(a)
+                        attractions.append(attraction.get())
         
+        numberOfAttractions = len(attractions)
+        attractionCount = 64
+        for attraction in attractions:
+            attractionCount = attractionCount + 1
+            if attractionCount < 91:
+                attraction.label = chr(attractionCount)
+            
         return attractions
     
     def get(self):
@@ -50,9 +46,13 @@ class SearchPage(Controller):
         search = self.request.get("q")
         coords = self.request.get("c")
         
+        template_values = {}
+        
         if coords:
             
-            url = "http://maps.google.com/maps/geo?q=%s&sensor=false" % coords
+            url = "http://maps.google.com/maps/geo?q=%s&sensor=false" % urllib.quote(coords)
+            
+            template_values['coords'] = coords
             
             jsonString = urllib.urlopen(url).read()
             if jsonString:
@@ -60,79 +60,49 @@ class SearchPage(Controller):
                 try:
                     lat = data['Placemark'][0]['Point']['coordinates'][1]
                     lon = data['Placemark'][0]['Point']['coordinates'][0]
+                    template_values['attractions'] = self.getAttractions(lat, lon)
                     try:
-                        template_values = {
-                            'search': "%s, %s" % (
-                                data['Placemark'][0]['AddressDetails']['Country']['AdministrativeArea']['SubAdministrativeArea']['SubAdministrativeAreaName'],
-                                data['Placemark'][0]['AddressDetails']['Country']['CountryName']
-                            ),
-                            'coords': coords,
-                            'attractions': self.getAttractions(lat, lon)
-                        }
-                    except KeyError:
-                        template_values = {
-                            'coords': coords,
-                            'attractions': self.getAttractions(lat, lon)
-                        }
-                except KeyError:
-                    template_values = {
-                        'coords': coords,
-                        'error': True
-                    }
+                        template_values['search'] = "%s, %s" % (
+                            data['Placemark'][0]['AddressDetails']['Country']['AdministrativeArea']['SubAdministrativeArea']['SubAdministrativeAreaName'],
+                            data['Placemark'][0]['AddressDetails']['Country']['CountryName']
+                        )
+                    finally:
+                        pass
+                finally:
+                    pass
             else:
                 parts = coords.split(",")
                 lat = parts[0]
                 lon = parts[1]
-                template_values = {
-                    'coords': coords,
-                    'attractions': self.getAttractions(lat, lon)
-                }
-            
-            self.output('search.html', template_values)
+                template_values['attractions'] = self.getAttractions(lat, lon)
             
         else:
             
             url = "http://maps.google.com/maps/geo?q=%s&sensor=false" % urllib.quote(search)
             
+            template_values['search'] = search,
+            
             jsonString = urllib.urlopen(url).read()
             if jsonString:
                 data = json.loads(jsonString)
                 try:
                     lat = data['Placemark'][0]['Point']['coordinates'][1]
                     lon = data['Placemark'][0]['Point']['coordinates'][0]
+                    template_values['coords'] = "%.1f,%.1f" % (lat, lon)
                     if len(data['Placemark']) > 1:
-                        template_values = {
-                            'search': search,
-                            'coords': lat + ',' + lon,
-                            'results': data['Placemark']
-                        }
+                        template_values['results'] = data['Placemark']
                     else:
+                        template_values['attractions'] = self.getAttractions(lat, lon)
                         try:
-                            template_values = {
-                                'search': "%s, %s" % (
-                                    data['Placemark'][0]['AddressDetails']['Country']['AdministrativeArea']['SubAdministrativeArea']['SubAdministrativeAreaName'],
-                                    data['Placemark'][0]['AddressDetails']['Country']['CountryName']
-                                ),
-                                'coords': "%.1f,%.1f" % (lat, lon),
-                                'attractions': self.getAttractions(lat, lon)
-                            }
-                        except KeyError:
-                            template_values = {
-                                'search': search,
-                                'coords': lat + ',' + lon,
-                                'attractions': self.getAttractions(lat, lon)
-                            }
-                except KeyError:
-                    template_values = {
-                        'search': search,
-                        'error': True
-                    }
-            else:
-                template_values = {
-                    'search': search,
-                    'error': True
-                }
+                            template_values['search'] = "%s, %s" % (
+                                data['Placemark'][0]['AddressDetails']['Country']['AdministrativeArea']['SubAdministrativeArea']['SubAdministrativeAreaName'],
+                                data['Placemark'][0]['AddressDetails']['Country']['CountryName']
+                            )
+                        finally:
+                            pass
+                finally:
+                    pass
             
-            template_values['q'] = search
-            self.output('search.html', template_values)
+        template_values['q'] = search
+        self.output('search.html', template_values)
         
