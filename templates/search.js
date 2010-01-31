181475730cf4503c448db745c90231132a638135
Paul James <paul@peej.co.uk>
2010-01-31 22:24:29 +0000

    Fixed attribute strings in JSON format

--- templates/search.js
+++ templates/search.js
@@ -2,13 +2,13 @@
 {% if attractions %}[
     {% for attraction in attractions %}
 {
-    id: "{{ attraction.id|escape }}",
-    title: "{{ attraction.name|escape }}",
-    location: {
-        lat: {{ attraction.location.lat|escape }},
-        lon: {{ attraction.location.lon|escape }}
+    "id": "{{ attraction.id }}",
+    "title": "{{ attraction.name }}",
+    "location": {
+        "lat": {{ attraction.location.lat }},
+        "lon": {{ attraction.location.lon }}
     },
-    datetime: {{ attraction.datetime|date:"U" }}
+    "datetime": {{ attraction.datetime|date:"U" }}
 }{% if not forloop.last	%},{% endif %}
     {% endfor %}
 ]{% endif %}
