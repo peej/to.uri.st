bfc601cf2e0f434ba06ac58a61ee831ab5883ec0
Paul James <paul@peej.co.uk>
2010-02-02 21:35:33 +0000

    Added 404 for non-existent attraction

--- controllers/attraction.py
+++ controllers/attraction.py
@@ -10,11 +10,13 @@ class AttractionPage(Controller):
         attractions.filter("id =", attractionId)
         attraction = attractions.get()
         
-        attraction.picture = self.convertFlickrUrl(attraction.picture, 'm')
-        
-        template_values = {
-            'attraction': attraction
-        }
-        
-        self.output('attraction', 'html', template_values)
-        
+        if attraction:
+            attraction.picture = self.convertFlickrUrl(attraction.picture, 'm')
+            
+            template_values = {
+                'attraction': attraction
+            }
+            
+            self.output('attraction', 'html', template_values)
+        else:
+            self.output('404', 'html')
