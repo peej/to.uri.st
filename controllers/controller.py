c82abff7bdb956987f33eac3e3afaceebe5369a6
Paul James <paul@peej.co.uk>
2010-02-09 22:31:51 +0000

    Correct response codes for default controller behaviour

--- controllers/controller.py
+++ controllers/controller.py
@@ -261,9 +261,11 @@ class Controller(webapp.RequestHandler):
         elif os.path.exists('templates' + self.request.path + '.html'):
             self.output(self.request.path, 'html')
         else:
+            self.error(404)
             self.output('404', 'html')
     
     def handle_exception(self, exception, debug_mode):
         super(Controller, self).handle_exception(exception, debug_mode)
         if not debug_mode:
+            self.error(500)
             self.output('500', 'html')
