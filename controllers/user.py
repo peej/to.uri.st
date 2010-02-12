8e3766a820d658d6fbc051de3dc73d9bcf26ba91
Paul James <paul@peej.co.uk>
2010-02-12 19:22:23 +0000

    User page controller added

--- controllers/user.py
+++ controllers/user.py
@@ -6,9 +6,15 @@ class UserPage(Controller):
     
     def get(self, userid):
         
+        from google.appengine.api import users
+        user = users.get_current_user()
+        print user.nickname()
+        print user.user_id()
+        print user.email()
+        
         template_values = {
             'nickname': userid
         }
         
-        self.output('user', 'html', template_values)
+        #self.output('user', 'html', template_values)
         
