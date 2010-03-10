c9927e6e39daf5c8b796c5244628ae179293069f
Paul James <paul@peej.co.uk>
2010-03-10 21:31:29 +0000

    Updated to remove dupes in geoboxes

--- loader/dedupe.py
+++ loader/dedupe.py
@@ -3,16 +3,18 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from models.attraction import Attraction
+from models.geobox import GeoBox
 
 class DeDupeWorker(webapp.RequestHandler):
     def get(self):
         n = self.request.get('n') or 1
+        f = self.request.get('f') or 1
         
         taskqueue.add(
             url = '/dedupe',
             params = {
-                'n': n
+                'n': n,
+                'f': f
             }
         )
         
@@ -21,26 +23,32 @@ class DeDupeWorker(webapp.RequestHandler):
     def post(self):
         n = int(self.request.get('n'))
         l = 50
+        f = self.request.get('f')
         
         import csv
         
-        csvReader = csv.reader(open('attractions4.csv'))
+        csvReader = csv.reader(open('attractions' + f + '.csv'))
         
         count = 1
         for row in csvReader:
             
             if count >= n and count < n + l:
                 
-                attractions = Attraction.all()
-                attractions.filter("id =", row[0])
-                attractionObjects = attractions.fetch(50)
+                parts = row[1].split(',')
+                lat = round(float(parts[0]), 1)
+                lon = round(float(parts[1]), 1)
                 
-                deleteIt = False
-                for attraction in attractionObjects:
-                    if deleteIt:
-                        attraction.delete()
-                    else:
-                        deleteIt = True
+                query = GeoBox.all()
+                query.filter("lat =", lat)
+                query.filter("lon =", lon)
+                geoBox = query.get()
+                
+                if geoBox:
+                    keys = {}
+                    for e in geoBox.attractions:
+                        keys[e] = 1
+                    geoBox.attractions = keys.keys()
+                    geoBox.put()
                 
             elif count >= n:
                 
@@ -48,11 +56,21 @@ class DeDupeWorker(webapp.RequestHandler):
                     url = '/dedupe',
                     params = {
                         'n': n + l,
+                        'f': f
                     }
                 )
                 break
             
             count = count  + 1
+        
+        if f < 4:
+            taskqueue.add(
+                url = '/dedupe',
+                params = {
+                    'n': n + l,
+                    'f': f + 1
+                }
+            )
 
 def main():
     run_wsgi_app(webapp.WSGIApplication([
